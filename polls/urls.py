809e9e1250c97bd6024f238e691a372c51da11fe
Vihansa <vihansathilakrathne1999@gmail.com>
2023-08-10 17:09:20 -0700

    first commit

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = "polls"
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
