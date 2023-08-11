2dd16d8fedf174c9df1a8040e7b03dc321c4011b
Vihansa <vihansathilakrathne1999@gmail.com>
2023-08-11 12:16:57 -0700

    second commit

--- first_app/settings.py
+++ first_app/settings.py
@@ -81,8 +81,12 @@ LOGIN_URL = reverse_lazy('login')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'new_db',
+        'USER': 'db_user',
+        'PASSWORD': 'abcde',
+        'HOST': 'localhost', 
+        'PORT': '',  
     }
 }
 
